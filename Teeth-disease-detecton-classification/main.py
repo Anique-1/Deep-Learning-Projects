145bc1610cca7e41cabe764e67e4faf36cc2a63f
Muhammad Anique <165008080+Anique-1@users.noreply.github.com>
2024-12-08 13:13:21 +0500

    Add files via upload

---
+++ Teeth-disease-detecton-classification/main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+# Load the model
+model = load_model('teeth_disease_trained_model.h5')
+
+# Define class names
+class_names = ['Calculus', 'Gingivitis', 'Mouth Ulcer', 'Tooth Discoloration', 'caries', 'hypodontia']
+
+st.title("Teeth Disease Detection Using Convolution Neural Network(CNN)")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    input_img= tf.keras.utils.load_img(uploaded_file, target_size=(180,180 ))
+    input_img_array=tf.keras.utils.img_to_array(input_img)
+    img_array = np.expand_dims(np.array(input_img_array), axis=(0, -1))
+
+    prediction = model.predict(img_array)
+    predicted_class = class_names[np.argmax(prediction)]
+    confidence = round(np.max(prediction)*100)
+
+    st.image(input_img, caption=f"Predicted: {predicted_class} ({confidence:.2f})", use_column_width=True)
